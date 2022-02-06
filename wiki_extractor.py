9237089588f26f3fa91df2b47d6c70f66dc3fe23
Cheril Shah <79891166+Cheril311@users.noreply.github.com>
2022-02-06 19:05:54 +0530

    Update wiki_extractor.py

--- wiki_extractor.py
+++ wiki_extractor.py
@@ -1,6 +1,12 @@
 import argparse
 import wikipedia
 import json
+import wikipediaapi
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+wiki_wiki = wikipediaapi.Wikipedia('en')
  
  
 # Initialize parser
@@ -14,30 +20,35 @@ args = parser.parse_args()
 
 num_urls = int(args.num_urls)
 
-search_results = wikipedia.search(args.keyword, results = (num_urls + (num_urls//2)))
+search_results = wikipedia.search(args.keyword, results = (num_urls))
 
 dicti = {}
 results = []
+text = ''
 st = ''
 for i in search_results:
     
-    try:
-      text = wikipedia.page(i).content
-      for j in text:
+      page_py = wiki_wiki.page(i)
+      source = urlopen(page_py.fullurl).read() 
+      soup = BeautifulSoup(source,'lxml')
+      for paragraph in soup.find_all('p'):
+        text += paragraph.text
+      for j in text[1:]:
        if j.startswith("\n"):
         break
        else:
         st += j
-      dicti['url'] = wikipedia.page(i).url
+      st = re.sub(r'\[.*?\]+', '', st)
+      st = st.replace('\n', '')
+      st = st.replace(u'\xa0', u' ')
+      dicti['url'] = page_py.fullurl
       dicti['paragraph'] = st
-      results.append(dicti)
-    except :
-        continue
-    text = ''
-    st = ''
-    dicti ={}
+      results.append(dicti)        
+      text = ''
+      st = ''
+      dicti ={}
     
-results = results[:num_urls]
+
 
 with open(args.output, 'w') as outfile:
     json.dump(results, outfile)
